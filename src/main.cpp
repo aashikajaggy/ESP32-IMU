f20f5ca86d8d4f205dbad088e1feeeb079879fcf
AashikaJagadeesh <aj414@duke.edu>
2025-07-27 14:11:59 -0400

    identified IMU

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,18 @@
 #include <Arduino.h>
+#include <Wire.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
+const int MPU = 0x68; // address of MPU device 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  Wire.begin(15, 4);
+  Wire.beginTransmission(MPU);
+  Wire.write(0x6B); // device reset register
+  Wire.write(0x00); // the reset value is 0x00 for all registers except registers 107 and 117
+  Wire.endTransmission(true);
+
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
