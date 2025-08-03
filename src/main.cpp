589c0aea075c928ba85a45ea8a6ff587f02cc431
AashikaJagadeesh <aj414@duke.edu>
2025-08-03 17:16:16 -0400

    semi-functioning clock? haha

--- src/main.cpp
+++ src/main.cpp
@@ -1,97 +1,99 @@
 
-/*
-#include <Arduino.h>
-#include <Wire.h>
-
-
-float accx, accy, accz;
-const int MPU = 0x68; // address of MPU device 
-void setup() {
-  Serial.begin(115200);
-  Wire.begin(15, 4); // don't have to explicitly use the sda/scl pins on the esp32
-  Wire.beginTransmission(MPU);
-  Wire.write(0x6B); // device reset register
-  Wire.write(0x00); // the reset value is 0x00, bit 7 is the device reset and we are assigning this bit to zero
-  Wire.endTransmission(true); // START is enabled, SDA goes low while SCL is high. This is because I2C is an open drain protocol meaning that we a switch pulling the line down low 
-  // when communication start 
-
-}
-
-void loop() {
-  Wire.beginTransmission(MPU);
-  Wire.write(0x3B); 
-  Wire.endTransmission(false); // send another start bit to start the i2c communication 
-  Wire.requestFrom(MPU, 6); // start reading 6 bytes
-  accx = Wire.read() << 8 | Wire.read(); // combining the low byte and the high byte into one number
-  // everytime we call Wire.read() we are reading a byte
-  accx = accx / 16384.0; // for +- 2g sensitivity
-  accy = Wire.read() << 8 | Wire.read();
-  accy = accy / 16384.0; // for +- 2g sensitivity
-  accz = Wire.read() << 8 | Wire.read();
-  accz = accz / 16384.0; // for +- 2g sensitivity
-  Serial.print("accx");
-  Serial.println(accx);
-  Serial.print("accy");
-  Serial.println(accy);
-  Serial.print("accz");
-  Serial.println(accz);
-}
-*/
-
-
-
 #include <Arduino.h>
 #include <cstdint>
 
 const int mySDA = 21;
 const int mySCL = 22;
+const int pin4 = 4;
+const int freq = 100000;
+const int dutyCycle = 128;
+const int resolution = 8;
+int pwmChannel = 0;
+volatile int countPWMCycles = 0;
+volatile bool stoppingConditionReached = false;
 
+volatile bool pwmStarted = false;
+
+void stopPWM();
 void master_send_address(uint8_t address);  // forward declaration
+void IRAM_ATTR handleRisingEdge();
 
 void setup() {
+  // Setting up serial line baud rate
   Serial.begin(115200);
-  pinMode(mySDA, OUTPUT);
-  pinMode(mySCL, OUTPUT);
 
+  // start the clock on high
+  //pinMode(mySCL, OUTPUT);
+  //digitalWrite(mySCL, HIGH); 
+
+  //count the highs of pwm
+  pinMode(pin4, INPUT);
+
+
+  // Using the esp32 pwn library to create a 50% duty cycle pwn with a 100khz frequency. Resolution of 8 means that the resolution granularity is 256. 
+  ledcSetup(pwmChannel, freq, resolution);
+
+  attachInterrupt(pin4, handleRisingEdge, RISING);
+
+
+
+  // signal the start condition
+  pinMode(mySDA, OUTPUT);
+  digitalWrite(mySDA, LOW);
+  delayMicroseconds(5);
+
+  // configure the timer
+  //timer = timerBegin(0, 80, true);
+  //timerAttachInterrupt(timer, &stopPWM, true);
 }
 
-//test the input_pullup first 
-//test that we can make an accurate clock with 9 cycles
-
 void loop() {
-  pinMode(mySDA, INPUT_PULLUP);
-  pinMode(mySCL, INPUT_PULLUP);
-  delayMicroseconds(5);
-  master_send_address(104);
-  delay(1000);
+  if(stoppingConditionReached) {
+    ledcDetachPin(mySCL);
+    pinMode(mySCL, OUTPUT);
+    digitalWrite(mySCL, HIGH); 
+    countPWMCycles = 0;
+    pwmStarted = false;
+    stoppingConditionReached = false;
+    Serial.println("caught the stopping condition");
+    //delay(1000);
+  }
+
+  if(!pwmStarted) {
+    ledcAttachPin(mySCL, pwmChannel);
+    ledcWrite(pwmChannel, dutyCycle);
+    pwmStarted = true;
+  }
+
+  // we want one iteration of loop to be a complete iteration of 9 SCL cycles 
+  /*
+  if(!pwmStarted) {
+    ledcAttachPin(mySCL, pwmChannel);
+    ledcWrite(pwmChannel, dutyCycle);
+    timerAlarmWrite(timer, 90, false);
+    timerAlarmEnable(timer);
+    pwmStarted = true;
+  }
+
+  if(!cycleDone) {
+
+  }
+  
+  if(cycleDone) {
+    delay(1000);
+    cycleDone = false;
+    pwmStarted = false;
+  }
+    */
 }
 
 void master_send_address(uint8_t address) {
-  //start condition
-  digitalWrite(mySDA, LOW);
-  delayMicroseconds(5);
-  int curr_bit = 0;
-  for(int i=0; i<8; i++){
-    digitalWrite(mySCL, LOW);
-    curr_bit = (address >> (7 - i)) & 1;
-    if(curr_bit == 1) {
-      digitalWrite(SDA, HIGH);
-    } else {
-      digitalWrite(SDA, LOW);
-    }
-    delayMicroseconds(5);
-    digitalWrite(mySCL, HIGH);
-    delayMicroseconds(5);
-  }
-  // write bit
-  digitalWrite(mySCL, LOW);
-  digitalWrite(mySDA, LOW);
-  delayMicroseconds(5);
-  digitalWrite(mySCL, HIGH);
-  delayMicroseconds(5);
-  digitalWrite(mySCL, LOW);
-  delayMicroseconds(5);
 }
 
-
-
+void IRAM_ATTR handleRisingEdge() {
+  countPWMCycles++;
+  if(countPWMCycles==9) {
+    ledcWrite(pwmChannel, 0);
+    stoppingConditionReached = true;
+  }
+}
